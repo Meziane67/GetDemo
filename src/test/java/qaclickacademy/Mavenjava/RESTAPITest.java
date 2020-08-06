1007277ff6c153499287ed6601fbd3ea9bfc9f75
Meziane <meziane67@yahoo.com>
2020-08-06 15:43:43 -0400

    This is comit from Asiam time zone guy

--- src/test/java/qaclickacademy/Mavenjava/RESTAPITest.java
+++ src/test/java/qaclickacademy/Mavenjava/RESTAPITest.java
@@ -6,6 +6,10 @@ public class RESTAPITest {
 	@Test
 	public void postjira() {
 		System.out.println("postjira");
+		System.out.println("postjira");
+		System.out.println("postjira2");
+		System.out.println("postjira3");
+	
 	}
 
 	@Test
