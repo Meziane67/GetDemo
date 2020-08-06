a7510b751300cbed65e45a9947c7756280ef855a
Meziane <meziane67@yahoo.com>
2020-08-06 19:27:11 -0400

    American guy comit

--- src/test/java/qaclickacademy/Mavenjava/RESTAPITest.java
+++ src/test/java/qaclickacademy/Mavenjava/RESTAPITest.java
@@ -6,11 +6,18 @@ public class RESTAPITest {
 	@Test
 	public void postjira() {
 		System.out.println("postjira");
-		System.out.println("postjira");
+		System.out.println("postjira1");
 		System.out.println("postjira2");
+
+
 		System.out.println("postjira3");
+		System.out.println("postjira4");
+		System.out.println("postjira5");
+		
 	
 	}
+	
+
 
 	@Test
 	public void deleteTwitter() {
